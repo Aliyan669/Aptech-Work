bb996d7f99699be4521221e5b3a99f3ebad6e076
Aliyan Amir <tboy02722@gmail.com>
2024-05-21 04:04:01 +0500

    Html Css Js

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-alert("Welcome")
+// alert("Welcome")  // Dialog Box 
+// confirm("Hello World!")   // Confirm Dialog Box 
+// prompt("Enter a Name","Aliyan")   // User Prompt Dialog Box
+// console.log("Hello World")   // Print on Console
+// document.write("Hello World!")   // Print on Screen
+
+//////   Variables in Js   //////
+
+var name = "Aliyan"   // Variable for String
+var num = 65   // Variable for Number
+// console.log(name)
+
+
+/////    Three Types of Variables    //////
+
+// var name = "Aliyan"
+// let name = "Aliyan"
+// const name = "Aliyan"
+
+
+/////    Arithmetic Operators in Js    //////
+   
+var number = 22 + 30    /// Addition
+console.log("Addition  >>>",number)
+
+var number = 50 - 30    /// Subtract 
+console.log("Subtract  >>>",number)
+
+var number = 2 * 20     /// Multiply
+console.log("Multiply  >>>",number)
+
+var number = 2 / 20     /// Division
+console.log("Division  >>>",number)
+
+var number = 2 % 20     /// Modulus
+console.log("Modulus  >>>",number)
+
+var numm = 2
+numm++    /// Increament
+console.log("Increament  >>>",numm)
+
+var nummm = 5
+nummm--   /// Decrement
+console.log("Decrement  >>>",nummm)
+
+
+/////    Concatenating on Text String   //////
+
+var firstName = "Aliyan ";
+var lastName = "Amir" ;
+console.log(firstName + lastName)
+
+
+/////      //////
