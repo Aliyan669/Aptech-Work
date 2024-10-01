8bc6bb51bbc54922dc49faf9d52d0c47c00706e7
Aliyan Amir <tboy02722@gmail.com>
2024-10-02 02:29:05 +0500

    Learning mySQL

--- Learning mySQL/query.sql
+++ Learning mySQL/query.sql
@@ -1,2 +1,14 @@
+-- //////  All mySQL Query  ///////
+
+--  Install Wamp Server (Step by Step)
+-- https://youtu.be/M2at7D-lciw?si=pCAPj-9sHDd16y5P
+
 -- /// Create Database ///
 CREATE DATABASE Aliyan_data;    
+
+-- /// Create Table ///
+CREATE TABLE Student (
+    student_id int,
+    student_id varchar(50),
+    student_email varchar(50)
+);
