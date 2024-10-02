3d5828b0610dad1064640ee1071b86bfa171a672
Aliyan Amir <tboy02722@gmail.com>
2024-10-03 02:07:50 +0500

    Learning mySQL

--- Learning mySQL/query.sql
+++ Learning mySQL/query.sql
@@ -1,14 +1,28 @@
 -- //////  All mySQL Query  ///////
-
 --  Install Wamp Server (Step by Step)
 -- https://youtu.be/M2at7D-lciw?si=pCAPj-9sHDd16y5P
 
 -- /// Create Database ///
-CREATE DATABASE Aliyan_data;    
+CREATE DATABASE Aliyan_data;
 
 -- /// Create Table ///
 CREATE TABLE Student (
-    student_id int,
-    student_id varchar(50),
-    student_email varchar(50)
+        student_id int,
+        student_name varchar(50),
+        student_email varchar(50)
+);
+
+-- /// Insert Data on Table ///
+INSERT INTO Student (student_id, student_name, student_email)
+VALUES (1, 'Aliyan', "aliyanamir@gmail.com"),
+       (2, 'Fahad', "fahad@gmail.com")
+
+
+-- /// Read Data on Table ///
+SELECT * FROM Student;
+
+
+-- /// Add Primary Key & Auto_Increament on Creating Table ///
+CREATE TABLE Student (
+        student_id int PRIMARY KEY AUTOINCREMENT,
 );
