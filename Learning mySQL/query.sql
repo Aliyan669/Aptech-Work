52797a10acb47e6d8623ff066df440a1a8e7c0b3
Aliyan Amir <tboy02722@gmail.com>
2024-10-19 02:21:56 +0500

    Learning MySQL

--- Learning mySQL/query.sql
+++ Learning mySQL/query.sql
@@ -1,6 +1,37 @@
--- //////  All MySQL Query  ///////
+-- MySQL is the world’s most popular open source database. 
+-- According to DB-Engines, MySQL ranks as the second-most-popular database, behind Oracle Database. 
+-- MySQL powers many of the most accessed applications, including Facebook, Twitter, Netflix, Uber, Airbnb, Shopify, and Booking.com.
+-- MySQL is a relational database management system
+-- MySQL is open-source
+-- MySQL was first released in 1995
+-- MySQL is developed, distributed, and supported by Oracle Corporation
+-- MySQL is named after co-founder Monty Widenius's daughter: My
+-- MySQL is ideal for both small and large applications
+-- MySQL is very fast, reliable, scalable, and easy to use
+-- MySQL is cross-platform
+-- Data Looks like Excel Sheet (rows and column) and tables
+
 --  Install Wamp Server (Step by Step)
 -- https://youtu.be/M2at7D-lciw?si=pCAPj-9sHDd16y5P
+
+--  Install mysql server, mysql client (workbench), mysql shell
+--  https://www.youtube.com/watch?v=2om3byn2lxs
+--  https://www.youtube.com/watch?v=GwHpIl0vqY4
+
+-- Database file location
+-- C:\ProgramData\MySQL\MySQL Server 8\data
+
+-- Database Import in Mysql workbench
+-- https://www.youtube.com/watch?v=7Cbm5vPQvNI
+
+
+-- # Import Below sample database
+
+-- # Sample Database 1
+-- # https://github.com/mdanish0320/teaching-class/blob/master/JP-BE-PY-batch-1/mysql-class/sample-databases/e-commerce-vehicles/vehicle-store-db.sql
+
+-- # Sample Database 2
+-- # https://github.com/mdanish0320/teaching-class/tree/master/JP-BE-PY-batch-1/mysql-class/sample-databases/e-commerce-movies-rental
 
 
 -- /// Create Database ///
@@ -47,6 +78,32 @@ UNIQUE       -- (Ensures that all values in a column are different)
 DEFAULT      -- (Sets a default value for a column if no value is specified)
 
 
+-- /// Arithmetic Operators MySQL ///
++  -- (Add)
+-  -- (Subtract)
+*  -- (Multiply)
+/  -- (Divide)
+%  -- (Modulos)
+
+
+-- /// Comparison Operators MySQL ///
+=  -- (Equal to)
+>  -- (Greater than)
+<  -- (Less than)
+>= -- (Greater than or equal to)
+<= -- (Less than or equal to)
+
+
+-- /// Logical Operators MySQL ///
+AND  -- (TRUE if all the conditions separated by AND is TRUE)
+OR   -- (TRUE if any of the conditions separated by OR is TRUE)
+NOT  -- (Displays a record if the condition(s) is NOT TRUE)
+IN   -- (TRUE if the operand is equal to one of a list of expressions)
+LIKE -- (TRUE if the operand matches a pattern)
+BETWEEN  -- (TRUE if the operand is within the range of comparisons)
+EXISTS  -- (TRUE if the subquery returns one or more records)
+
+
 -- /// Read Data on Table ///
 SELECT * FROM TABLE_NAME;
 
@@ -55,8 +112,24 @@ SELECT * FROM TABLE_NAME;
 DROP TABLE TABLE_NAME;
 
 
--- /// Delete Only Data on Table ///
+-- /// Delete All Data on Table ///
 TRUNCATE TABLE TABLE_NAME;  --(Delete All Data on Existing Table)
+
+
+-- /// Add Coulumn on Table ///
+ALTER TABLE TABLE_NAME ADD COLUMN COLUMN_NAME Datatype CONSTRAINT;
+
+
+-- /// Delete Coulumn on Table ///
+ALTER TABLE TABLE_NAME DROP COLUMN COLUMN_NAME;
+
+
+-- /// Change Coulumn on Table ///
+ALTER TABLE TABLE_NAME CHANGE COLUMN Old_Name New_Name New_Datatype New_Constraint;
+
+
+-- /// Rename Table on MySQL ///
+ALTER TABLE TABLE_NAME RENAME TO  NEW_TABLE_NAME ;
 
 
 -- /// Insert Data on Table ///
@@ -112,11 +185,17 @@ SELECT MAX(COLUMN_NAME) FROM TABLE_NAME;  --(Demo)
 SELECT * FROM TABLE_NAME LIMIT 2;
 
 
+---- Offset Limit on MySQL  ----
+SELECT * FROM TABLE_NAME LIMIT 10 OFFSET 0;
+SELECT * FROM TABLE_NAME LIMIT 10 OFFSET 10;
+SELECT * FROM TABLE_NAME LIMIT 10 OFFSET 20;
+
+
 ---- Distinct on MySQL ----
 SELECT DISTINCT COLUMN_NAME FROM TABLE_NAME;
 
 
----- Where on MySQL ----
+---- Where Clause on MySQL ----
 SELECT * FROM TABLE_NAME WHERE COLUMN_NAME = 100;
 
 
@@ -136,3 +215,15 @@ SELECT COLUMN_NAME_1, COUNT(COLUMN_NAME_2) FROM TABLE_NAME GROUP BY COLUMN_NAME_
 
 ---- Having Clause on MySQL  ----
 SELECT COLUMN_NAME_1, COUNT(COLUMN_NAME_2) FROM TABLE_NAME GROUP BY COLUMN_NAME_1 HAVING COUNT(COLUMN_NAME_2) > 5;
+
+
+---- Alias on MySQL  ----
+SELECT firstname AS fname, lastname AS lname FROM employees;
+
+
+---- Union on MySQL  ----
+SELECT COLUMN_NAME FROM TABLE_1 UNION SELECT COLUMN_NAME FROM TABLE_1;
+
+
+---- Union All on MySQL  ----
+SELECT COLUMN_NAME FROM TABLE_1 UNION All SELECT COLUMN_NAME FROM TABLE_1;
