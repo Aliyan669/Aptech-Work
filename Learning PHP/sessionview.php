ecee8125e3981ababb22d8bec0db8b088359eef0
Aliyan Amir <tboy02722@gmail.com>
2024-11-20 00:56:16 +0500

    Php Cookies & Session

---
+++ Learning PHP/sessionview.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    // Echo session variables that were set on previous page
+    echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+    ?>
+
+</body>
+
+</html>
